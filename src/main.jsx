1103b9c8e57eb65ecd3b80bacbe45a9e2e0d4a56
Mario Henrique Velho Sacomori <d.mariohenrique41@gmail.com>
2026-06-23 14:12:17 -0400

    Limpando marcadores de conflito

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-<<<<<<< HEAD
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-=======
 import './liquid-theme.css'
 import App from './App.jsx'
 import { ThemeProvider } from './ThemeContext.jsx'
@@ -17,6 +10,5 @@ createRoot(document.getElementById('root')).render(
     <ThemeProvider>
       <App />
     </ThemeProvider>
->>>>>>> 9f00b5e (ajustes de temas)
   </StrictMode>,
 )
